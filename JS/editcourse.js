89ef9bad7ac548a4e0dbd143aa4f4362b73e4885
Daniel Yoh <dyoh52@gmail.com>
2021-11-15 14:34:36 -0500

    Updated

---
+++ JS/editcourse.js
@@ -0,0 +1,7 @@
+document.getElementById("editbtt").addEventListener("click", function () {
+  location.href = "https://dyoh0.github.io/chalkboard/HTML/selectedcourse.html";
+});
+
+document.getElementById("leavebtt").addEventListener("click", function () {
+  location.href = "https://dyoh0.github.io/chalkboard/HTML/index.html";
+});
