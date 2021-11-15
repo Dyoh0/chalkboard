9782b2913fbf0fe5eab44be6a902241ae86bad45
Daniel Yoh <dyoh52@gmail.com>
2021-11-15 15:06:17 -0500

    Updated link

--- JS/gradelist.js
+++ JS/gradelist.js
@@ -2,6 +2,6 @@ var list = document.getElementsByClassName("button");
 
 for (let i = 0; i < list.length; i++) {
   list[i].addEventListener("click", function () {
-    location.href = "https://dyoh0.github.io/chalkboard/HTML/grade.html";
+    location.href = "https://dyoh0.github.io/chalkboard/HTML/grading.html";
   });
 }
