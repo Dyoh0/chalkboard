43fe93061eb96c82f3236fd1834226d565b7d62d
Daniel Yoh <dyoh52@gmail.com>
2021-11-15 15:05:08 -0500

    Added grade view

---
+++ JS/gradelist.js
@@ -0,0 +1,7 @@
+var list = document.getElementsByClassName("button");
+
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", function () {
+    location.href = "https://dyoh0.github.io/chalkboard/HTML/grade.html";
+  });
+}
